cd8a380f30fb27f6d64297b360fb22cbd414c1aa
linkwechat <linkwechat@foxmail.com>
2016-11-23 21:03:13 +0800

    通用工具集

---
+++ src/main/java/com/linkwechat/commons/util/MathUtils.java
@@ -0,0 +1,73 @@
+package com.linkwechat.commons.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * 数学工具类
+ * 
+ * @author linkwechat linkwechat@foxmail.com
+ * @version 1.0
+ */
+public class MathUtils {
+
+    private static final Log logger = LogFactory.getLog(MathUtils.class);
+
+    /**
+     * 获取[n, m]区间内的随机数
+     * 
+     * @param n
+     * @param m
+     * @return long
+     */
+    public static long getRandom(long n, long m) {
+        if (n > m) {
+            long temp = n;
+            n = m;
+            m = temp;
+        }
+        return (long) (n + Math.random() * (m - n + 1));
+    }
+
+    /**
+     * 求两个数最大公约数
+     * 
+     * @param n
+     * @param m
+     * @return long
+     */
+    public static long commonDivisor(long n, long m) {
+        // 辗转相除是用大的除以小的。如果n<m，第一次相当n与m值交换
+        while (n % m != 0) {
+            long temp = n % m;
+            n = m;
+            m = temp;
+        }
+        return m;
+    }
+
+    /**
+     * 求两个数最小公倍数
+     * 
+     * @param n
+     * @param m
+     * @return long
+     */
+    public static long commonMultiple(long n, long m) {
+        return n * m / commonDivisor(n, m);
+    }
+
+    /**
+     * 求多个数最小公倍数
+     * 
+     * @param n
+     * @return long
+     */
+    public static long commonMultiple(long... n) {
+        long value = n[0];
+        for (int i = 1; i < n.length; i++) {
+            value = commonMultiple(value, n[i]);
+        }
+        return value;
+    }
+}
---
+++ src/main/java/com/linkwechat/commons/util/StringUtils.java
@@ -0,0 +1,183 @@
+package com.linkwechat.commons.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * 字符串工具类
+ * 
+ * @author linkwechat linkwechat@foxmail.com
+ * @version 1.0
+ */
+public class StringUtils {
+
+    private static final Log logger = LogFactory.getLog(StringUtils.class);
+
+    /**
+     * 检查字符串是否为null或空字符串""，如果为空, 则返回true。
+     * 
+     * <pre>
+     * StringUtil.isEmpty(null)      = true
+     * StringUtil.isEmpty("")        = true
+     * StringUtil.isEmpty(" ")       = false
+     * StringUtil.isEmpty("bob")     = false
+     * StringUtil.isEmpty("  bob  ") = false
+     * </pre>
+     * 
+     * @param str
+     *            要检查的字符串
+     * @return boolean
+     */
+    public static boolean isEmpty(final String str) {
+        return str == null || str.length() == 0;
+    }
+
+    /**
+     * 检查字符串是否不为null或空字符串""，如果不为空, 则返回true。
+     * 
+     * @param str
+     *            要检查的字符串
+     * @return boolean
+     */
+    public static boolean isNotEmpty(final String str) {
+        return !isEmpty(str);
+    }
+
+    /**
+     * 检查字符串是否是空白：null、空字符串""或只有空白字符，如果为空白，则返回true。
+     * 
+     * <pre>
+     * StringUtil.isBlank(null)      = true
+     * StringUtil.isBlank("")        = true
+     * StringUtil.isBlank(" ")       = true
+     * StringUtil.isBlank("bob")     = false
+     * StringUtil.isBlank("  bob  ") = false
+     * </pre>
+     * 
+     * @param str
+     *            要检查的字符串
+     * @return boolean
+     */
+    public static boolean isBlank(final String str) {
+        int length;
+
+        if (str == null || (length = str.length()) == 0) {
+            return true;
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (!Character.isWhitespace(str.charAt(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 检查字符串是否不是空白：null、空字符串""或只有空白字符，如果不为空白，则返回true。
+     * 
+     * @param str
+     *            要检查的字符串
+     * @return boolean
+     */
+    public static boolean isNotBlank(final String str) {
+        return !isBlank(str);
+    }
+
+    /**
+     * 空格字符填充
+     * 
+     * @param length
+     *            需要填充的长度
+     * @return String
+     */
+    public static String fillBlankChar(int length) {
+        return fillChar(' ', length);
+    }
+
+    /**
+     * 字符填充
+     * 
+     * @param c
+     *            需要填充的字符
+     * @param length
+     *            需要填充的长度
+     * @return String
+     */
+    public static String fillChar(char c, int length) {
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < length; i++) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    /***
+     * 
+     * 十六进制字符解码成数值
+     * 
+     * @param c
+     *            十六进制字符
+     * @return 十六进制字符表示的数值
+     */
+    private static int HexDecode(char c) {
+        if (c >= 'a')
+            return (c - 'a' + 10) & 0x0f;
+        if (c >= 'A')
+            return (c - 'A' + 10) & 0x0f;
+        return (c - '0') & 0x0f;
+    }
+
+    /***
+     * 
+     * 十六进制数值编码成字符
+     * 
+     * @param i
+     *            十六进制数值
+     * @return 十六进制数值表示的字符
+     */
+    private static char HexEncode(int i) {
+        i = i & 0x0f;
+        if (i >= 10)
+            return (char) (i + 'A' - 10);
+        return (char) (i + '0');
+    }
+
+    /***
+     * 
+     * 从十六进制字符串到字节数组转换
+     * 
+     * @param hexStr
+     *            十六进制字符串
+     * @return 字节数组
+     */
+    public static byte[] HexStringToBytes(String hexStr) {
+        byte[] b = new byte[hexStr.length() / 2];
+        int j = 0;
+        for (int i = 0; i < b.length; i++) {
+            char c0 = hexStr.charAt(j++);
+            char c1 = hexStr.charAt(j++);
+            b[i] = (byte) ((HexDecode(c0) << 4) | HexDecode(c1));
+        }
+        return b;
+    }
+
+    /***
+     * 
+     * 从字节数组到十六进制字符串转换
+     * 
+     * @param data
+     *            字节数组
+     * @return 十六进制字符串
+     */
+    public static String BytesToHexString(byte[] data) {
+        char[] a = new char[data.length * 2];
+        int j = 0;
+        for (int i = 0; i < data.length; i++) {
+            a[j++] = HexEncode(data[i] >> 4 & 0x0f);
+            a[j++] = HexEncode(data[i] & 0x0f);
+        }
+        return new String(a);
+    }
+}
